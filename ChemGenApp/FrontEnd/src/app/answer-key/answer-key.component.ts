a5c0fd58b58d748eaacc10f6017e0c8ed2fc9590
bjackson14 <bjackson14@ggc.edu>
2018-09-19 03:16:27 -0400

    Added instructions at the top and answer key to the right of form. CHEM-4

---
+++ ChemGenApp/FrontEnd/src/app/answer-key/answer-key.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-answer-key',
+  templateUrl: './answer-key.component.html',
+  styleUrls: ['./answer-key.component.scss']
+})
+export class AnswerKeyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
