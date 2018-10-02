e1d063e90c312a42b3f4832ec02cb7692a374722
bjackson14 <bjackson14@ggc.edu>
2018-10-01 21:57:32 -0400

    Created model for answer key and setup communication between the two components CHEM-27

--- ChemGenApp/FrontEnd/src/app/app.component.ts
+++ ChemGenApp/FrontEnd/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AnswerKey } from './models/answer-key.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FrontEnd';
+export class AppComponent implements OnInit {
+
+  // Holds data for child components
+  private answer: AnswerKey = new AnswerKey();
+
+  ngOnInit() {
+    this.answer.anion1 = '';
+    this.answer.cation1 = '';
+    this.answer.anion2 = '';
+    this.answer.cation2 = '';
+  }
 }
---
+++ ChemGenApp/FrontEnd/src/app/models/answer-key.model.ts
@@ -0,0 +1,11 @@
+// Answer key to be used to hold the data inputed by the professors
+// and the answers calculated by the application
+export class AnswerKey {
+    anion1: String;
+    cation1: String;
+    grams1: number;
+    anion2: String;
+    cation2: String;
+
+    constructor() { }
+}
